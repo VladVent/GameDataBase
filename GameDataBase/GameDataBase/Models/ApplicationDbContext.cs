0f58e4719b9f077b13db5763d9993f1685ccf32b
VladVent <vlad070898@gmail.com>
2018-04-03 04:16:12 +0300

    GameDataBase

---
+++ GameDataBase/GameDataBase/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameDataBase.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+      /// public DbSet<data> DataBase { get; set; }
+    }
+}
+
---
+++ GameDataBase/GameDataBase/Models/ModelFolder/GameData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameDataBase.Models.ModelFolder
+{
+    public class GameData
+    {
+        public static void GameDa(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices
+                .GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if(!context.Database.Any())
+            {
+                context.Database.AddRange(
+                    new Game
+                    {
+                        Name = "Silent Hill",
+                        Descriprion = "Перша частина гри",
+                        Developer = "Team Silent",
+                        Publisher = "Konami",
+                        Rating = 78,
+                        Category = "Horror"
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
